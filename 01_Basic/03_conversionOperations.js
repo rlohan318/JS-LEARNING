719dca0e0e6b1ee126694d8ee1275b509dc462b7
rlohan318 <87542443+rlohan318@users.noreply.github.com>
2024-04-15 16:42:32 +0000

    conversion of dataTypes

---
+++ 01_Basic/03_conversionOperations.js
@@ -0,0 +1,34 @@
+let score = "false"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0
+*/
+
+// let isLoggedIn = "abc"
+// let boolenIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof(boolenIsLoggedIn))
+// console.log(boolenIsLoggedIn)
+
+
+// 1 => true; 0 => false
+// "" =>false
+// "abc" => true
+
+
+let someNumber = 33
+let stringSomeNumber = String(someNumber)
+console.log(typeof (stringSomeNumber));
+console.log(stringSomeNumber);
+
